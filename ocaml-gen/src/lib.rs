33277a203d1e9b6af8c8fa3b82a8014f504dd4f8
David Wong <davidwong.crypto@gmail.com>
2022-09-12 15:03:10 -0700

    update README

--- ocaml-gen/src/lib.rs
+++ ocaml-gen/src/lib.rs
@@ -1,5 +1,5 @@
 #![deny(missing_docs)]
-#![doc = include_str!("../README.md")]
+#![doc = include_str!("../../README.md")]
 
 extern crate ocaml_derive;
 use std::collections::{hash_map::Entry, HashMap};

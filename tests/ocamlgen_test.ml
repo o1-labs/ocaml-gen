1dad40cecbc4e2375c96575c8e422f3daad45680
Danny Willems <be.danny.willems@gmail.com>
2023-05-15 16:40:25 +0200

    Introduce QCheck to test bindings

--- tests/ocamlgen_test.ml
+++ tests/ocamlgen_test.ml
@@ -2,17 +2,21 @@ let test_single_tuple_access_field () =
   let b = Bindings.new_t () in
   assert (b.inner = "Hello")
 
-let test_built_in_type_int32 () =
-  let s1 = Int32.of_int 3 in
-  let s2 = Int32.of_int 6 in
-  assert (Int32.(add s1 s2) = Bindings.test_add_i32 s1 s2)
+let test_built_in_type_int32 =
+  QCheck.(
+    Test.make ~name:"Test builtin type int32 with addition"
+      (tup2 QCheck.int32 QCheck.int32) (fun (i1, i2) ->
+        Int32.(add i1 i2) = Bindings.test_add_i32 i1 i2 ) )
 
 let () =
+  let builtin_types_qcheck_suite =
+    List.map QCheck_alcotest.to_alcotest [ test_built_in_type_int32 ]
+  in
   let open Alcotest in
   run "Test binding generations"
     [ ( "single_tuple"
       , [ test_case "Access field and check value" `Quick
             test_single_tuple_access_field
         ] )
-    ; ("builtin types", [ test_case "add i32" `Quick test_built_in_type_int32 ])
+    ; ("Builtin types", builtin_types_qcheck_suite)
     ]

ce1119fff4ef05dfe0b4bc45b59331433835645d
dannywillems <dannywillems@users.noreply.github.com>
2025-04-15 12:52:00 +0000

    deploy: b6a98d3d0b94358d973f88a8f214a23ffc314155

---
+++ trait.impl/core/marker/trait.UnsafeUnpin.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["ocaml_gen",[["impl UnsafeUnpin for <a class=\"struct\" href=\"ocaml_gen/struct.Env.html\" title=\"struct ocaml_gen::Env\">Env</a>",1,["ocaml_gen::Env"]]]],["ocamlgen_test_stubs",[["impl UnsafeUnpin for <a class=\"struct\" href=\"ocamlgen_test_stubs/struct.Car.html\" title=\"struct ocamlgen_test_stubs::Car\">Car</a>",1,["ocamlgen_test_stubs::Car"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"ocamlgen_test_stubs/struct.SingleTuple.html\" title=\"struct ocamlgen_test_stubs::SingleTuple\">SingleTuple</a>",1,["ocamlgen_test_stubs::SingleTuple"]],["impl&lt;T&gt; UnsafeUnpin for <a class=\"struct\" href=\"ocamlgen_test_stubs/struct.Package.html\" title=\"struct ocamlgen_test_stubs::Package\">Package</a>&lt;T&gt;<div class=\"where\">where\n    T: UnsafeUnpin,</div>",1,["ocamlgen_test_stubs::Package"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[156,658]}
